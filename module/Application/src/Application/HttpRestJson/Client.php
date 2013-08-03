1d46b82903adfeab8813fc04ef48f44880babf9f
Steven Alexander <steven.william.alexander@googlemail.com>
2013-08-03 13:21:38 +0100

    added comment

--- module/Application/src/Application/HttpRestJson/Client.php
+++ module/Application/src/Application/HttpRestJson/Client.php
@@ -12,6 +12,7 @@ class Client
 		$request = new Request();
         $request->setUri('https://api.github.com/users/defunkt');
         $httpClient = new HttpClient();
+        # use curl adapter as standard has problems with ssl
         $httpClient->setAdapter('Zend\Http\Client\Adapter\Curl');
 
         $response = $httpClient->dispatch($request);
