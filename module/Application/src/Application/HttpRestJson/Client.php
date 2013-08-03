018b336aa94efd3cbb7e79d8d7303dac9ec28678
Steven Alexander <steven.william.alexander@googlemail.com>
2013-08-03 22:58:32 +0100

    added logic for post/put/delete

--- module/Application/src/Application/HttpRestJson/Client.php
+++ module/Application/src/Application/HttpRestJson/Client.php
@@ -4,6 +4,7 @@ namespace Application\HttpRestJson;
 
 use Zend\Http\Client as HttpClient;
 use Zend\Http\Request;
+use Zend\Stdlib\Parameters;
 
 class Client
 {
@@ -16,8 +17,33 @@ class Client
 
     public function get($url)
     {
-		$request = new Request();
+		return $this->dispatchRequestAndDecodeResponse($url, "GET");
+    }
+
+    public function post($url, $data)
+    {
+        return $this->dispatchRequestAndDecodeResponse($url, "POST", $data);
+    }
+
+    public function put($url, $data)
+    {
+        return $this->dispatchRequestAndDecodeResponse($url, "PUT", $data);
+    }
+
+    public function delete($url)
+    {
+        return $this->dispatchRequestAndDecodeResponse($url, "DELETE");
+    }
+
+    protected function dispatchRequestAndDecodeResponse($url, $method, $data = null)
+    {
+        $request = new Request();
         $request->setUri($url);
+        $request->setMethod($method);
+
+        if ($data){
+            $request->setPost(new Parameters($data));
+        }
 
         $response = $this->httpClient->dispatch($request);
         # should interogate response status, throwing appropiate exceptions for error codes
--- module/Application/test/ApplicationTest/HttpRestJson/ClientTest.php
+++ module/Application/test/ApplicationTest/HttpRestJson/ClientTest.php
@@ -6,6 +6,7 @@ use PHPUnit_Framework_Assert;
 
 use Application\HttpRestJson\Client as HttpRestJsonClient;
 use Zend\Http\Response;
+use Zend\Stdlib\Parameters;
 
 class ClientTest extends PHPUnit_Framework_TestCase
 {
@@ -13,20 +14,9 @@ class ClientTest extends PHPUnit_Framework_TestCase
     {
         $url = "http://api/test";
         $method = "GET";
-
         $responseContent = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
-        $response = new Response();
-        $response->setStatusCode(Response::STATUS_CODE_200);
-        $response->setContent($responseContent);
 
-        $mockHttpClient = $this->getMock('Zend\Http\Client', array('dispatch'));
-        $mockHttpClient->expects($this->once())
-                        ->method('dispatch')
-                        ->will($this->returnCallback(function($request) use ($response, $method, $url) {
-                            PHPUnit_Framework_Assert::assertSame($request->getMethod(), $method);
-                            PHPUnit_Framework_Assert::assertSame($request->getUriString(), $url);
-                            return $response;
-                        }));
+        $mockHttpClient = $this->getMockForSuccessfulHttpClientDispatch($url, $method, $responseContent);
 
         $httpRestJsonClient = new HttpRestJsonClient($mockHttpClient);
 
@@ -34,5 +24,87 @@ class ClientTest extends PHPUnit_Framework_TestCase
             json_decode($responseContent, true),
             $httpRestJsonClient->get($url)
         );
+    }
+
+    public function testPostReturnsAssociativeArrayContent()
+    {
+        $url = "http://api/test";
+        $method = "POST";
+        $responseContent = '{"id":1}';
+        $data = array("active" => true, "val" => "test");
+
+        $mockHttpClient = $this->getMockForSuccessfulHttpClientDispatch(
+            $url,
+            $method,
+            $responseContent,
+            $data
+        );
+
+        $httpRestJsonClient = new HttpRestJsonClient($mockHttpClient);
+
+        $this->assertSame(
+            json_decode($responseContent, true),
+            $httpRestJsonClient->post($url, $data)
+        );
+    }
+
+    public function testPutReturnsAssociativeArrayContent()
+    {
+        $url = "http://api/test";
+        $method = "PUT";
+        $responseContent = '{"id":1,"active":true,"val":"test"}';
+        $data = array("id" => 1, "active" => true, "val" => "test");
+
+        $mockHttpClient = $this->getMockForSuccessfulHttpClientDispatch(
+            $url,
+            $method,
+            $responseContent,
+            $data
+        );
+
+        $httpRestJsonClient = new HttpRestJsonClient($mockHttpClient);
+
+        $this->assertSame(
+            json_decode($responseContent, true),
+            $httpRestJsonClient->put($url, $data)
+        );
+    }
+
+    public function testDeleteReturnsAssociativeArrayContent()
+    {
+        $url = "http://api/test/1";
+        $method = "DELETE";
+        $responseContent = '{"data":"id 1 has been deleted"}';
+
+        $mockHttpClient = $this->getMockForSuccessfulHttpClientDispatch($url, $method, $responseContent);
+
+        $httpRestJsonClient = new HttpRestJsonClient($mockHttpClient);
+
+        $this->assertSame(
+            json_decode($responseContent, true),
+            $httpRestJsonClient->delete($url)
+        );
+    }
+
+    protected function getMockForSuccessfulHttpClientDispatch($url, $method, $responseContent, $postData = null)
+    {
+        $response = new Response();
+        $response->setStatusCode(Response::STATUS_CODE_200);
+        $response->setContent($responseContent);
+
+        $mockHttpClient = $this->getMock('Zend\Http\Client', array('dispatch'));
+        $mockHttpClient->expects($this->once())
+                        ->method('dispatch')
+                        ->will($this->returnCallback(function($request) use ($response, $method, $url, $postData) {
+                            PHPUnit_Framework_Assert::assertSame($request->getMethod(), $method);
+                            PHPUnit_Framework_Assert::assertSame($request->getUriString(), $url);
+
+                            if ($postData) {
+                                PHPUnit_Framework_Assert::assertSame($request->getPost()->getArrayCopy(), $postData);
+                            }
+                            return $response;
+                        }));
+
+        return $mockHttpClient;
     }
 }
